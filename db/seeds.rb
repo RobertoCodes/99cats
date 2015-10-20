a8689bc4838e4f4205724899018393e009011db6
JimmyAndRob <JimmyAndRob@appacademy.com>
2015-10-20 11:56:21 -0400

    Add index and show methods and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cat.destroy_all
+
+Cat.create!(birth_date: "10-16-1991", color: :brown, name: "Robbie", sex: "m", description: "coolest cat")
+Cat.create!(birth_date: "05-03-1991", color: :black, name: "Jimmy", sex: "m", description: "most studious cat")
+Cat.create!(birth_date: "01-01-1900", color: :grey, name: "Clint", sex: "f", description: "oldest cat")
