fcc3943c94a0862218ebe1af3699a5a1e0c17534
JimmyAndRob <JimmyAndRob@appacademy.com>
2015-10-20 18:01:32 -0400

    Add validation overlapping requests.

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,15 +1,37 @@
 class CatRentalRequest < ActiveRecord::Base
 
+  validate :overlapping_approved_requests
 
-# def overlapping_requests
-#   @overlaps = Cat.joins(:cat_rental_requests).where.not
-#   ("cat_rental_requests.cat_id = ? AND cat_rental_requests.start_date < ? AND cat_rental_requests.end_date > ?",
-#    cat_rental_params[:cat_id], cat_rental_params[:start_date], car_rental_params[:end_date] )
-# end
+
+def overlapping_requests
+  # @overlaps = Cat.joins(:cat_rental_requests).where.not("cat_rental_requests.cat_id = ?
+  #  AND ? < cat_rental_requests.start_date OR cat_rental_requests.end_date < ?",
+  #  self.cat_id, self.end_date, self.start_date)
+  CatRentalRequest
+    .where("(:id IS NULL) OR id != :id", id: self.id)
+    .where("cat_id = ?", self.cat_id)
+    .where
+    .not("? < cat_rental_requests.start_date OR cat_rental_requests.end_date < ?", self.end_date, self.start_date)
+end
 
 def overlapping_approved_requests
+  result = overlapping_requests.where("cat_rental_requests.status = 'APPROVED'")
+  unless result.empty?
+    errors[:overlapping_requests] << "Rental request dates overlap with another rental"
+  end
+end
 
+def overlapping_pending_requests
+  overlapping_requests.where("cat_rental_requests.status = 'PENDING'")
+end
 
+def approve!
+  self.status = "APPROVED"
+  self.update!
+  overlapping_pending_requests.each do |request|
+    request.status = "DENIED"
+    request.update!
+  end
 end
 
 belongs_to :cat,
@@ -17,7 +39,6 @@ belongs_to :cat,
   foreign_key: :cat_id,
   primary_key: :id,
   class_name: "Cat"
-
 
 
 
